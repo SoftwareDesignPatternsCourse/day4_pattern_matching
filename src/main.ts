d1f3b20042829260c8372c0a9e31f992cdce197d
Facundo Merighi <merighifacundo@gmail.com>
2023-05-25 16:00:37 -0300

    second

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,43 @@
-// Enum representing the types of running statistics
-enum RunningStatType {
-    MEAN,
-    MIN,
-    MAX,
-    COUNT,
+enum BookType {
+  PAPER = 'PAPER',
+  ELECTRONIC_TYPE = 'ELECTRONIC_TYPE',
+  NEWSLETTER = 'NEWSLETTER'
+}
+
+
+
+class Book {
+  amount:number;
+  type:BookType;
+
+  constructor(amount:number, type:BookType) {
+    this.amount = amount;
+    this.type = type;
   }
-  
-  // Model class representing a running statistic
-  class RunningStat {
-    private type: RunningStatType;
-    private value: number;
-  
-    constructor(type: RunningStatType, value: number) {
-      this.type = type;
-      this.value = value;
-    }
-  
-    getType(): RunningStatType {
-      return this.type;
-    }
-  
-    getValue(): number {
-      return this.value;
-    }
-  }
-  
-  // Model class representing a collection of running statistics
-  class RunningStats {
-    private stats: RunningStat[];
-  
-    constructor() {
-      this.stats = [];
-    }
-  
-    addStat(stat: RunningStat): void {
-      this.stats.push(stat);
-    }
-  
-    printStats(): void {
-      for (const stat of this.stats) {
-        console.log(`${stat.getType()}: ${stat.getValue()}`);
-      }
-    }
-  }
-  
-  // Example usage
-  const runningStats = new RunningStats();
-  
-  const meanStat = new RunningStat(RunningStatType.MEAN, 8.5);
-  const minStat = new RunningStat(RunningStatType.MIN, 5.2);
-  const maxStat = new RunningStat(RunningStatType.MAX, 12.9);
-  const countStat = new RunningStat(RunningStatType.COUNT, 100);
-  
-  runningStats.addStat(meanStat);
-  runningStats.addStat(minStat);
-  runningStats.addStat(maxStat);
-  runningStats.addStat(countStat);
-  
-  runningStats.printStats();
-  
+}
+
+
+const getData = ():Book[] => {
+  const books:Book[] = [];
+  books.push(new Book(10, BookType.ELECTRONIC_TYPE))
+  books.push(new Book(20, BookType.ELECTRONIC_TYPE))
+  books.push(new Book(30, BookType.ELECTRONIC_TYPE))
+  books.push(new Book(40, BookType.ELECTRONIC_TYPE))
+  books.push(new Book(10, BookType.PAPER))
+  books.push(new Book(10, BookType.PAPER))
+  books.push(new Book(10, BookType.PAPER))
+  books.push(new Book(10, BookType.PAPER))
+  books.push(new Book(10, BookType.NEWSLETTER))
+  books.push(new Book(20, BookType.NEWSLETTER))
+  books.push(new Book(30, BookType.NEWSLETTER))
+  return books;
+}
+
+
+const main = () => {
+
+
+  console.log("the book store");
+
+
+}
